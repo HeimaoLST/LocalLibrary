3096dae5a2f1870f21a9c2bdd4530f590e3681f7
HeimaoLST <linlinwayou@qq.com>
2024-09-26 14:41:18 +0800

    Local Library

---
+++ test.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// 导入控制器模块
+const book_controller = require("./controllers/bookController");
+const author_controller = require("./controllers/authorController");
+const genre_controller = require("./controllers/genreController");
+const book_instance_controller = require("./controllers/bookinstanceController");
+
+console.log(typeof(book_controller.index));
